ead5490eb2ba6327685a2fed61e1b4f6b5a22b37
reynardamadeus <reynard.starlee@gmail.com>
2025-05-06 12:08:40 +0700

    lesson-material get

--- app/Http/Controllers/LessonMaterialController.php
+++ app/Http/Controllers/LessonMaterialController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LessonMaterial;
 use Illuminate\Http\Request;
 
 class LessonMaterialController extends Controller
 {
-    //
+    public function get(){
+        try{
+            $lm = LessonMaterial::paginate(5);
+            return response()->json([
+                'data' => $lm
+            ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                    "message" => "internal server error",
+                    "errors" => $e->getMessage()
+                ],500);
+        }
+    }
 }
